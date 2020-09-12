2ffac276fb14cf4bd89b43f9c2414c95f77b5b26
haidarvm <haidarvm@gmail.com>
2020-09-13 06:27:06 +0700

    repair

--- mark/start.php
+++ mark/start.php
@@ -1,5 +1,6 @@
 <?php
 use Mark\App;
+use ActiveRecord\ActiveDatabase;
 
 require 'vendor/autoload.php';
 
@@ -7,12 +8,28 @@ $api = new App('http://0.0.0.0:3000');
 
 $api->count = 4; // process count
 
+function mactive() {
+    $db_config = [
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => 'hai2coders',
+        'database' => 'stack',
+        'dbdriver' => 'mysqli',
+        'pconnect' => false,
+        'db_debug' => true
+    ];
+
+    // Add Config and give it a name
+    ActiveDatabase::addConfig('read', $db_config);
+    $db = ActiveDatabase::get('read');
+    return $db;
+}
 
 function p_query($sql) {
     // $database = $db == 'NULL' ? 'news' : $db;
     $db_connection = pg_connect('host=localhost dbname=stack user=postgres password=hai2coders');
     $query = pg_query($db_connection, $sql);
-    if(!$query) {
+    if (!$query) {
         echo "An error occurred.\n";
         return false;
     }
@@ -32,56 +49,93 @@ function query($sql) {
     return $conn->query($sql);
 }
 
-
 $api->any('/', function ($requst) {
     return 'Hello haidar';
 });
 
-$api->get('/pes', function ($requst) {
-    $sql = 'SELECT body FROM posts where "id" =45';
-    $result = p_query($sql);
-    $json = json_encode(pg_fetch_all($result));
-    return $json;
-});
 
 $api->get('/ps', function ($requst) {
-    $number = rand(1, 116193);
-    $sql = 'SELECT body FROM posts_clean where id ='.$number;
-    $sqlr = 'SELECT body FROM posts_clean ORDER BY random() LIMIT 5';
+    $sql = 'SELECT "Body" FROM "Posts"  where "Id" =565';
     $result = p_query($sql);
-    if ($result) {
-        $array = array_column(pg_fetch_all($result), 'body');
-        $resultArray = array_map('strip_tags', $array);
-        $json = json_encode($resultArray);
-        return $json;
-    }
-    return null;
+    $array = pg_fetch_all($result);
+    return json_encode($array);
 });
 
 $api->get('/pr', function ($requst) {
     $number = rand(1, 382745);
-    $sql = "SELECT p.\"Body\" FROM \"Posts\" p INNER JOIN \"PostsId\" i ON i.post_id = p.\"Id\"  where autoid =".$number;
+    $sql = 'SELECT * FROM "Posts" p INNER JOIN "PostsId" i ON i.post_id = p."Id"  where autoid =' . $number;
     $result = p_query($sql);
-    if ($result) {
-        $array = array_column(pg_fetch_all($result), 'Body');
-        $resultArray = array_map('strip_tags', $array);
-        $json = json_encode($resultArray);
-        return $json;
-    }
-    return null;
+    $array = pg_fetch_all($result);
+    return json_encode($array);
+});
+
+$api->get('/par', function ($requst) {
+    $db_config = [
+        'hostname' => 'localhost',
+        'username' => 'postgres',
+        'password' => 'hai2coders',
+        'database' => 'stack',
+        'dbdriver' => 'postgre',
+        'pconnect' => false,
+        'db_debug' => true
+    ];
+
+    // Add Config and give it a name
+    ActiveDatabase::addConfig('read', $db_config);
+    $db = ActiveDatabase::get('read');
+    $number = rand(1, 382745);
+    $db->join('PostsId', 'Posts.Id = PostsId.post_id', 'inner');
+    $query = $db->get_where('Posts', ['autoid' => $number]);
+    $row = $query->row();
+    return json_encode($row);
+});
+
+$api->get('/mar', function ($requst) {
+    $db_config = [
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => 'hai2coders',
+        'database' => 'stack',
+        'dbdriver' => 'mysqli',
+        'pconnect' => false,
+        'db_debug' => true
+    ];
+
+    // Add Config and give it a name
+    ActiveDatabase::addConfig('read', $db_config);
+    $db = ActiveDatabase::get('read');
+    $number = rand(1, 382745);
+    $db->join('PostsId', 'Posts.Id = PostsId.post_id', 'inner');
+    $query = $db->get_where('Posts', ['autoid' => $number]);
+    $row = $query->row();
+    return json_encode($row);
 });
 
 $api->get('/mr', function ($requst) {
     $number = rand(1, 382745);
-    $sql = "SELECT p.Body FROM Posts p INNER JOIN PostsId i ON i.post_id = p.Id  where autoid =".$number;
+    $sql = 'SELECT p.Body FROM Posts p INNER JOIN PostsId i ON i.post_id = p.Id  where autoid =' . $number;
     $result = query($sql);
-    if ($result) {
-        $array = array_column($result->fetch_all(MYSQLI_ASSOC), 'Body');
-        $resultArray = array_map('strip_tags', $array);
-        $json = json_encode($resultArray);
-        return $json;
-    }
-    return null;
+    return json_encode($result->fetch_all(MYSQLI_ASSOC));
+});
+
+$api->get('/ms', function ($requst) {
+    $db_config = [
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => 'hai2coders',
+        'database' => 'stack',
+        'dbdriver' => 'mysqli',
+        'pconnect' => false,
+        'db_debug' => true
+    ];
+
+    // Add Config and give it a name
+    ActiveDatabase::addConfig('read', $db_config);
+    $db = ActiveDatabase::get('read');
+    $number = rand(1, 382745);
+    $query = $db->get_where('Posts', ['Id' => 565]);
+    $row = $query->row();
+    return json_encode($row->Body);
 });
 
 $api->get('/hello/{name}', function ($requst, $name) {
@@ -89,7 +143,7 @@ $api->get('/hello/{name}', function ($requst, $name) {
 });
 
 $api->post('/user/create', function ($requst) {
-    return json_encode(['code'=>0 ,'message' => 'ok']);
+    return json_encode(['code' => 0, 'message' => 'ok']);
 });
 
 $api->start();
